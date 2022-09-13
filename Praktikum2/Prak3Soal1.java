d4cedc215c931dddb43b59849e6a143452635d5a
Teuku indris <103029533+Indris2003@users.noreply.github.com>
2022-09-13 11:43:57 +0700

    Create Prak3Soal1.java

---
+++ Praktikum2/Prak3Soal1.java
@@ -0,0 +1,13 @@
+public class Prak3Soal1 {
+    public static void main(String args[]) {
+       float a = 3;
+       float b = 2;
+       
+       System.out.println("a + b = " + (a + b));
+       System.out.println("a - b = " + (a - b));
+       System.out.println("a * b = " + (a * b));
+       System.out.println("a / b = " + (a / b));
+       System.out.println("a % b = " + (a % b));
+     }
+    }
+    
